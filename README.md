a454c4b3aa920b5863917e22951acb4a0b642cfb
geckrDhrff <594277937@qq.com>
2023-10-29 16:57:12 +0800

    refactor:use proxmoxer to get pve info

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-利用telegram 监控PVE 主机状态
+利用telegram bot 监控PVE 主机状态
 
 参考文档
 https://link.zhihu.com/?target=https%3A//github.com/sshuangliu/Proxmox-VE-api
--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+import json
 import logging
-from pve import Pve_api
+
+from proxmoxer import ProxmoxAPI
 from html import escape
 from uuid import uuid4
 
@@ -7,16 +9,28 @@ from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
 from telegram.constants import ParseMode
 from telegram.ext import Application, CommandHandler, ContextTypes, InlineQueryHandler
 
-import os 
-import json
+import os
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 bot_token = os.getenv('BOT_TOKEN')
-pve_user = os.getenv('PVE_USER',default = "root@pam")
-pve_pwd = os.getenv('PVE_PWD',default="123456")
-pve_ip = os.getenv('PVE_IP', default = "127.0.0.1")
+pve_ip = os.getenv('PVE_IP', default="127.0.0.1")
+pve_user = os.getenv('PVE_USER', default="root@pam")
+pve_pwd = os.getenv('PVE_PWD', default="123456")
 
-PVE = Pve_api(ip=pve_ip, username=pve_user, password=pve_pwd)
-PVE.get_ticket()
+pve_token = os.getenv('PVE_TOKEN_NAME')
+pve_token_value = os.getenv('PVE_TOKEN_VALUE')
+
+# two ways to login pve
+# 1. username and password, the only requirement is to make a request within 2 hours
+# prox = ProxmoxAPI(pve_ip, pve_user, password=pve_pwd, verify_ssl=False)
+# 2. user, token_name and token_value, The API Token allows stateless interaction
+
+proxmox = ProxmoxAPI(
+    pve_ip, user=pve_user, token_name=pve_token,
+    token_value=pve_token_value, verify_ssl=False
+)
 
 # Enable logging
 logging.basicConfig(
@@ -32,25 +46,35 @@ logger = logging.getLogger(__name__)
 # context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
-    if len(PVE.ticket) > 0:
-        await update.message.reply_text("Hi!")
-    else:
-        await update.message.reply_text("Init Failed!")
-        
+    await update.message.reply_text("Hi!")
+
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+    await update.message.reply_text("/start - start \n \
+                                    /help - show help \n \
+                                    /get_status - get pve cluster status \n \
+                                    /get_nodes - get all nodes \n \
+                                    /get_resources - get pve cluster resources")
 
-async def getStatus(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+async def get_status(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
-    data = json.dumps(PVE.pve_status()['data']['cpuinfo'])
-    if len(data) > 4096:
-        for x in range(0, len(data), 4096):
-            await update.message.reply_text(data[x:x+4096])
-    else:
-        await update.message.reply_text(data)
-            
+    data = json.dumps(proxmox.cluster.status.get(), indent=4, separators=(',', ':'))
+    await update.message.reply_text(data)
+
+
+async def get_nodes(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    data = json.dumps(proxmox.nodes.get(), indent=4, separators=(',', ':'))
+    await update.message.reply_text(data)
+
+
+async def get_resources(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    data = json.dumps(proxmox.cluster.resources.get(), indent=4, separators=(',', ':'))
+    await update.message.reply_text(data)
+
 
 async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Handle the inline query. This is run when you type: @botusername <query>"""
@@ -90,9 +114,12 @@ def main() -> None:
     application = Application.builder().token(bot_token).build()
 
     # on different commands - answer in Telegram
+    # command should be low letter!
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("command1", getStatus))
+    application.add_handler(CommandHandler("get_status", get_status))
+    application.add_handler(CommandHandler("get_nodes", get_nodes))
+    application.add_handler(CommandHandler("get_resources", get_resources))
 
     # on inline queries - show corresponding inline results
     application.add_handler(InlineQueryHandler(inline_query))
